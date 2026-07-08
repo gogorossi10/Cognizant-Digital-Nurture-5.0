8d3fb64d1c02f3628ea0cbb732b242ce11e58bac
gogorossi10 <gokul.sn2005@gmail.com>
2026-07-08 15:38:54 +0530

    commit this change

--- Week-03-Spring-REST-using-Spring-Boot/HandsOn-07-REST-Get-Country-By-Code/spring-learn/src/main/java/com/cognizant/springlearn/service/CountryService.java
+++ Week-03-Spring-REST-using-Spring-Boot/HandsOn-07-REST-Get-Country-By-Code/spring-learn/src/main/java/com/cognizant/springlearn/service/CountryService.java
@@ -1,5 +1,13 @@
 package com.cognizant.springlearn.service;
 
+import com.cognizant.springlearn.Country;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class CountryService {
 
     @SuppressWarnings("unchecked")
