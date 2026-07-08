5ca53b48e512e3194967b016f156681679f18083
gogorossi10 <gokul.sn2005@gmail.com>
2026-07-09 00:11:48 +0530

    commit this change

--- Week-04-Microservices/HandsOn-01-Creating-Microservices/account/src/main/java/com/cognizant/account/controller/AccountController.java
+++ Week-04-Microservices/HandsOn-01-Creating-Microservices/account/src/main/java/com/cognizant/account/controller/AccountController.java
@@ -1,4 +1,14 @@
 package com.cognizant.account.controller;
 
 public class AccountController {
+
+    @GetMapping("/accounts/{number}")
+    public Account getAccount(@PathVariable String number) {
+
+        return new Account(
+                number,
+                "savings",
+                234343
+        );
+    }
 }
